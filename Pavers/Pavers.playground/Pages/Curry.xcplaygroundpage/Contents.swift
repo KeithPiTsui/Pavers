c82b0d474ad58daa5ec160171ab81ee81945fedc
keith <xiaofeng.xu@521qw.com>
2017-10-15 19:19:27 +0800

    remove curry function of one argument

--- Pavers/Pavers.playground/Pages/Curry.xcplaygroundpage/Contents.swift
+++ Pavers/Pavers.playground/Pages/Curry.xcplaygroundpage/Contents.swift
@@ -1,7 +1,18 @@
 //: [Previous](@previous)
 
 import Foundation
-import PaversFRP
+
+//public func curry<A, B>(_ function: @escaping (A) -> B) -> (A) -> B {
+//    return { (a: A) -> B in function(a) }
+//}
+
+public func curry<A, B, C>(_ function: @escaping (A, B) -> C) -> (A) -> (B) -> C {
+    return { (a: A) -> (B) -> C in { (b: B) -> C in function(a, b) } }
+}
+
+public func curry<A, B, C, D>(_ function: @escaping (A, B, C) -> D) -> (A) -> (B) -> (C) -> D {
+    return { (a: A) -> (B) -> (C) -> D in { (b: B) -> (C) -> D in { (c: C) -> D in function(a, b, c) } } }
+}
 
 struct A {
   let a: String
@@ -9,6 +20,6 @@ struct A {
 }
 
 let x = A.init
-//let cx = curry(x)
-
+let cx = curry(x)
+let a = cx("")("")
 //: [Next](@next)
--- Pavers/Sources/FRP/Functional/Utilities/Curry.swift
+++ Pavers/Sources/FRP/Functional/Utilities/Curry.swift
@@ -1,6 +1,6 @@
-public func curry<A, B>(_ function: @escaping (A) -> B) -> (A) -> B {
-    return { (a: A) -> B in function(a) }
-}
+//public func curry<A, B>(_ function: @escaping (A) -> B) -> (A) -> B {
+//    return { (a: A) -> B in function(a) }
+//}
 
 public func curry<A, B, C>(_ function: @escaping (A, B) -> C) -> (A) -> (B) -> C {
     return { (a: A) -> (B) -> C in { (b: B) -> C in function(a, b) } }
