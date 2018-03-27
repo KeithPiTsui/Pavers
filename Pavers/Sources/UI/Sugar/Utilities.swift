f080110ffd36a5c2981748627c627fb9edb569af
Keith <keith@KeithMacOS.local>
2018-03-27 18:43:16 +0800

    make framework static

--- Pavers/Sources/UI/Sugar/Utilities.swift
+++ Pavers/Sources/UI/Sugar/Utilities.swift
@@ -2,12 +2,12 @@ import UIKit
 
 public struct Utilities {
 
-  public static var isPushNotificationEnabled: Bool {
-    guard let settings = UIApplication.shared.currentUserNotificationSettings else {
-      return false
-    }
-
-    return UIApplication.shared.isRegisteredForRemoteNotifications
-      && !settings.types.isEmpty
-  }
+//  public static var isPushNotificationEnabled: Bool {
+//    guard let settings = UIApplication.shared.currentUserNotificationSettings else {
+//      return false
+//    }
+//
+//    return UIApplication.shared.isRegisteredForRemoteNotifications
+//      && !settings.types.isEmpty
+//  }
 }
